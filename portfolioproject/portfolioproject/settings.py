ca69b648105011460bcef4c31369ca6487ae23fb
Parth Tripathi <parth_08@outlook.com>
2026-02-06 01:40:10 +0530

    done

--- portfolioproject/portfolioproject/settings.py
+++ portfolioproject/portfolioproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portfolioproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template', 'static'],
+        'DIRS': ['templates', 'static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,4 +115,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
