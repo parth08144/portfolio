805d1b34682d9d0c02c9774278dea7a39e66b22c
Parth Tripathi <parth_08@outlook.com>
2026-02-07 01:52:15 +0530

    na

--- portfolioproject/core/views.py
+++ portfolioproject/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Contact
 
 def home_view(request):
     return render(request, "core/home.html")
@@ -9,8 +11,26 @@ def about_view(request):
 def projects_view(request):
     return render(request, "core/project.html")
 
-def contact_view(request):
+# def contact_view(request):
     return render(request, "core/contact.html")
 
+
+
+
+def contact_view(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        Contact.objects.create(
+            name=name,
+            email=email,
+            message=message
+        )
+
+        return redirect("contact")
+
+    return render(request, "core/contact.html")
 
 
